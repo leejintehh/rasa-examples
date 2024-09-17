76d7e7ccce319d9d685975764961d295caaa447d
leejintehh <110539446+leejintehh@users.noreply.github.com>
2024-09-17 22:43:50 +0800

    bot working, sql testing

--- 02-slots/actions/actions.py
+++ 02-slots/actions/actions.py
@@ -8,15 +8,16 @@ from rasa_sdk.executor import CollectingDispatcher
 class ActionSayShirtSize(Action):
 
     def name(self) -> Text:
-        return "action_say_shirt_size"
+        return "action_say_order"
 
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-        shirt_size = tracker.get_slot("shirt_size")
-        if not shirt_size:
-            dispatcher.utter_message(text="I don't know your shirt size.")
+        food_item = tracker.get_slot("food_item")
+        food_quantity = tracker.get_slot("food_quantity")
+        if not (food_item and food_quantity ):
+            dispatcher.utter_message(text="No order received, would you like to submit an order?")
         else:
-            dispatcher.utter_message(text=f"Your shirt size is {shirt_size}!")
+            dispatcher.utter_message(text=f"Your order is {food_quantity} {food_item}")
         return []
