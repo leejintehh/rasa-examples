540056284db3412534c764056a6a4048595e1dfb
leejintehh <110539446+leejintehh@users.noreply.github.com>
2024-09-19 02:10:13 +0800

    working with mysql server

--- 02-slots/actions/actions.py
+++ 02-slots/actions/actions.py
@@ -5,10 +5,10 @@ from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 
 
-class ActionSayShirtSize(Action):
+class ActionSayOrder(Action):
 
     def name(self) -> Text:
-        return "action_say_order"
+        return "action_say_current_order"
 
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
@@ -17,7 +17,39 @@ class ActionSayShirtSize(Action):
         food_item = tracker.get_slot("food_item")
         food_quantity = tracker.get_slot("food_quantity")
         if not (food_item and food_quantity ):
-            dispatcher.utter_message(text="No order received, would you like to submit an order?")
+            dispatcher.utter_message(text="No current order")
         else:
-            dispatcher.utter_message(text=f"Your order is {food_quantity} {food_item}")
+            dispatcher.utter_message(text=f"Your current order is {food_quantity} {food_item}, is this correct?")
+        return []
+
+
+
+#MySQL submit order to DB
+class ActionSubmitOrder(Action):
+
+    def name(self) -> Text:
+        return "action_submit_order"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        import mysql.connector
+
+        mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="password123",
+        database='rasa'
+        )
+
+        mycursor = mydb.cursor()
+
+        food_item = tracker.get_slot("food_item")
+        food_quantity = tracker.get_slot("food_quantity")
+        if food_item and food_quantity:
+            sql = "INSERT INTO orders (food, quantity) VALUES (%s, %s)"
+            val = (food_item, food_quantity)
+            mycursor.execute(sql, val)
+            mydb.commit()
+            dispatcher.utter_message(text=f"order submitted!")
         return []
